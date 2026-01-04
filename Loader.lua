bd25a22bd03473b31053207a52a372fb84179d78
ZlinSoul <discordaccaunt149@gmail.com>
2026-01-04 16:56:22 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,21 +1,45 @@
--- [[ Tesavek Public Loader ]] --
-local User = "KOTIK130" -- Твой ник на GitHub
-local Repo = "Roblox-script-test"   -- Название репозитория
-local Branch = "main"     -- Ветка (обычно main)
+-- [[ KOTIK130 Hub Loader ]] --
+-- Этот файл лежит в корне репозитория и запускает всё остальное.
 
+local User = "KOTIK130"
+local Repo = "Roblox-script-test"
+local Branch = "main"
+
+-- Функция для формирования ссылок на другие файлы в этом же репо
 local function GetURL(path)
     return string.format("https://raw.githubusercontent.com/%s/%s/%s/%s", User, Repo, Branch, path)
 end
 
--- Глобальная переменная для базы URL, чтобы модули тоже могли её использовать
-getgenv().RepoURL = function(path)
-    return GetURL(path)
-end
+-- Делаем эти функции доступными глобально для всех остальных модулей
+getgenv().RepoURL = GetURL
 
--- Глобальная функция импорта для модулей
 getgenv().Import = function(path)
-    return loadstring(game:HttpGet(GetURL(path)))()
+    local url = GetURL(path)
+    local success, result = pcall(function()
+        return game:HttpGet(url)
+    end)
+    
+    if not success then
+        warn("[Loader] Failed to fetch module: " .. path)
+        return function() end 
+    end
+    
+    local func, err = loadstring(result)
+    if not func then
+        warn("[Loader] Syntax error in module: " .. path .. " | Error: " .. tostring(err))
+        return function() end
+    end
+    
+    return func()
 end
 
--- Запуск
-loadstring(game:HttpGet(GetURL("Main.lua")))()
+-- Теперь, когда функции Import готовы, запускаем Main.lua
+-- Main.lua уже будет использовать этот же Import для загрузки вкладок
+local mainUrl = GetURL("Main.lua")
+local success, err = pcall(function()
+    loadstring(game:HttpGet(mainUrl))()
+end)
+
+if not success then
+    warn("[Loader] Critical Error loading Main.lua: " .. tostring(err))
+end
