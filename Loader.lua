c13d4fb3c82a2e616e31d225a7bd9d3ef1ad8563
ZlinSoul <discordaccaunt149@gmail.com>
2026-01-06 03:03:20 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,32 +2,21 @@
 local User = "KOTIK130"
 local Repo = "Roblox-script-test"
 local Branch = "main"
+local FileName = "OneFile.lua" -- Теперь грузим этот файл
 
-local function GetURL(path)
-    return string.format("https://raw.githubusercontent.com/%s/%s/%s/%s?t=%d", User, Repo, Branch, path, os.time())
+local url = string.format("https://raw.githubusercontent.com/%s/%s/%s/%s?t=%d", User, Repo, Branch, FileName, os.time())
+
+local success, result = pcall(function()
+    return game:HttpGet(url)
+end)
+
+if success then
+    loadstring(result)()
+else
+    warn("[Loader] Failed to load script: " .. url)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "Loader Error";
+        Text = "Check console (F9)";
+        Duration = 5;
+    })
 end
-
-getgenv().RepoURL = GetURL
-
-getgenv().Import = function(path)
-    local url = GetURL(path)
-    local success, result = pcall(function()
-        return game:HttpGet(url)
-    end)
-    
-    if not success then
-        warn("[Loader] Failed to fetch module: " .. path)
-        return function() end 
-    end
-    
-    local func, err = loadstring(result)
-    if not func then
-        warn("[Loader] Syntax error in module: " .. path .. " | Error: " .. tostring(err))
-        return function() end
-    end
-    
-    return func()
-end
-
-local mainUrl = GetURL("Main.lua")
-loadstring(game:HttpGet(mainUrl))()
